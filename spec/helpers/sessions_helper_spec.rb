c0c9294c05e19192d40e9b5a544fec6cceb5adf0
ktr11 <kenmcz315@gmail.com>
2022-06-30 23:06:18 +0900

    test: #23 sessions_helperのテストを補強

--- spec/helpers/sessions_helper_spec.rb
+++ spec/helpers/sessions_helper_spec.rb
@@ -13,5 +13,23 @@ require 'rails_helper'
 #   end
 # end
 RSpec.describe SessionsHelper, type: :helper do
-  pending "add some examples to (or delete) #{__FILE__}"
+  describe 'log_inメソッドのテスト' do
+    it 'log_inメソッドに渡したuserとセッションのidが等しければOK' do
+      user = create(:user)
+      log_in(user)
+      expect(session[:user_id]).to eq user.id
+      match(logged_in?)
+    end
+  end
+  describe 'current_userメソッドのテスト' do
+    it 'current_userメソッドの結果とログインユーザーが等しければOK' do
+      user = create(:user)
+      user2 = create(:user)
+      log_in(user)
+      expect(current_user).to eq user
+      expect(current_user).not_to eq user2
+      match(current_user?(user))
+      match(current_user?(user2))
+    end
+  end
 end
