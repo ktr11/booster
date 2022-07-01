55d7a164015e207176dbab6ecbaf7baed2edb726
ktr11 <kenmcz315@gmail.com>
2022-07-01 11:59:01 +0900

    test: #25 ユーザー登録処理のテストを追加

--- spec/requests/users_spec.rb
+++ spec/requests/users_spec.rb
@@ -13,4 +13,86 @@ RSpec.describe 'Users', type: :request do
       expect(response.body).to include('パスワード(確認)')
     end
   end
+  describe 'POST /signup/' do
+    it 'name,email,passwordが正しい場合、User登録成功' do
+      user_count = User.count
+      post '/signup/', params: {
+        user: {
+          name: 'test1',
+          email: 'test1@example.com',
+          password: 'testpass1',
+          password_confirmation: 'testpass1'
+        }
+      }
+      expect(response).to have_http_status(302)
+      is_expected.to redirect_to('/')
+      expect(session[:user_id]).to_not eq nil
+      get '/'
+      expect(response.body).to include('ログイン済')
+      expect(response.body).to include('ユーザー登録が完了しました')
+      user = User.find_by(email: 'test1@example.com')
+      expect(user).to_not eq nil
+      expect(user_count + 1).to eq User.count
+    end
+    it 'nameが空の場合、User登録失敗' do
+      user_count = User.count
+      post '/signup/', params: {
+        user: {
+          name: '',
+          email: 'test1@example.com',
+          password: 'testpass1',
+          password_confirmation: 'testpass1'
+        }
+      }
+      expect(response).to have_http_status(200)
+      expect(session[:user_id]).to eq nil
+      expect(response.body).to include('ユーザー名を入力してください')
+      expect(user_count).to eq User.count
+    end
+    it 'emailが空の場合、User登録失敗' do
+      user_count = User.count
+      post '/signup/', params: {
+        user: {
+          name: 'test1',
+          email: '',
+          password: 'testpass1',
+          password_confirmation: 'testpass1'
+        }
+      }
+      expect(response).to have_http_status(200)
+      expect(session[:user_id]).to eq nil
+      expect(response.body).to include('メールアドレスを入力してください')
+      expect(user_count).to eq User.count
+    end
+    it 'passwordが空の場合、User登録失敗' do
+      user_count = User.count
+      post '/signup/', params: {
+        user: {
+          name: 'test1',
+          email: 'test1@example.com',
+          password: '',
+          password_confirmation: 'testpass1'
+        }
+      }
+      expect(response).to have_http_status(200)
+      expect(session[:user_id]).to eq nil
+      expect(response.body).to include('パスワードを入力してください')
+      expect(user_count).to eq User.count
+    end
+    it 'password_confirmが空の場合、User登録失敗' do
+      user_count = User.count
+      post '/signup/', params: {
+        user: {
+          name: 'test1',
+          email: 'test1@example.com',
+          password: 'testpass1',
+          password_confirmation: ''
+        }
+      }
+      expect(response).to have_http_status(200)
+      expect(session[:user_id]).to eq nil
+      expect(response.body).to include('パスワード(確認)とパスワードの入力が一致しません')
+      expect(user_count).to eq User.count
+    end
+  end
 end
