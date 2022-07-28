0dc74e2a1b5879a8e984b082315d0f4167065e08
ktr11 <kenmcz315@gmail.com>
2022-07-28 20:46:13 +0900

    fix: #45 includeでよいので修正

--- spec/requests/users_spec.rb
+++ spec/requests/users_spec.rb
@@ -8,9 +8,9 @@ RSpec.describe 'Users', type: :request do
     it '200になり想定どおりの文字列が含まれる' do
       get '/signup/'
       expect(response).to have_http_status(200)
-      expect(response.body).to match(%r{ユーザー名})
-      expect(response.body).to match(%r{メールアドレス})
-      expect(response.body).to match(%r{パスワード})
+      expect(response.body).to include('ユーザー名')
+      expect(response.body).to include('メールアドレス')
+      expect(response.body).to include('パスワード')
       expect(response.body).to include('パスワード(確認)')
     end
   end
